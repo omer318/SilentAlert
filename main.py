51ef7f2517d15822505695769ca695e9638748ea
omer318 <domer154@gmail.com>
2023-11-04 00:09:39 +0200

    implemented

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import webbrowser
 import os
+import tzevaadom  # kudos to https://github.com/itaiguli/python-pikudHaoref-api
 
 
-def open_alert_window(filename):
+def alerts_handler(list_alerts):
+    for alert in list_alerts:
+
+        message = "New Alarm: " + \
+            alert["name_en"] + ". Zone: " + alert["zone_en"]
+        print(message)
+        open_web_window('./index.html')
+
+
+def open_web_window(filename):
     webbrowser.open('file://' + os.path.realpath(filename))
 
+
+def listen_on_alerts(areas):
+    tzevaadom.alerts_listener(alerts_handler,areas)
+
+
 def main():
-    open_alert_window('./index.html')
+    listen_on_alerts([])
+
 
 if __name__ == "__main__":
     main()
