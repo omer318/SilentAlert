befda2b86c8624b256da82eeed4d953bb81f8285
omer318 <domer154@gmail.com>
2023-11-04 00:29:34 +0200

    good idea, time consuming in practice

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import webbrowser
 import os
 import tzevaadom  # kudos to https://github.com/itaiguli/python-pikudHaoref-api
 
+AREAS = ["ראשון לציון - מזרח", "תעשיון צריפין"]
 
 def alerts_handler(list_alerts):
     for alert in list_alerts:
@@ -17,11 +18,11 @@ def open_web_window(filename):
 
 
 def listen_on_alerts(areas):
-    tzevaadom.alerts_listener(alerts_handler,areas)
+    tzevaadom.alerts_listener(alerts_handler, areas)
 
 
 def main():
-    listen_on_alerts([])
+    listen_on_alerts(AREAS)
 
 
 if __name__ == "__main__":
