04eb847439cd890df7582a11616368517f51d55c
omer318 <domer154@gmail.com>
2023-11-03 23:40:13 +0200

    added

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import webbrowser
+import os
 
+
+def open_alert_window(filename):
+    webbrowser.open('file://' + os.path.realpath(filename))
 
 def main():
-    print("hello")
+    open_alert_window('./index.html')
 
 if __name__ == "__main__":
     main()
